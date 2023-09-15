7f2d68a2a19a429b25f06d621aed88fc0d1d387b
Aditia Winanda <aditiaw1507@gmail.com>
2023-09-15 10:11:31 +0700

    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'helloWorld C2';
+  nim = '202102310';
+  nama = 'Aditia  Winanda';
+}
